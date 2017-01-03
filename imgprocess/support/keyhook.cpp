23f92c70a55a9ebcb19723462cea325f9614c16d
wuyougongzi <1211385701@qq.com>
2017-01-04 00:01:42 +0800

    添加写注册表项，为了支持开机自启动，第一次需要管理员权限运行

--- imgprocess/support/keyhook.cpp
+++ imgprocess/support/keyhook.cpp
@@ -1,12 +1,46 @@
 #include "Windows.h"
 #include <iostream>
+#include <tchar.h>
 using namespace std;
 
 HHOOK hookHandle;
 
 LRESULT CALLBACK keyHandler(int nCode, WPARAM wParam, LPARAM lParam);
 
+void startUp(LPCTSTR path)
+{
+	BOOL isWOW64;
+	REGSAM p;
+	IsWow64Process(GetCurrentProcess(),&isWOW64);
+	if (isWOW64){
+		p = KEY_WRITE | KEY_WOW64_64KEY;
+	}
+	else{
+		p = KEY_WRITE;
+	}
+	HKEY hKey;
+	LONG createRt = RegCreateKeyEx(HKEY_LOCAL_MACHINE, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"), 0, NULL, 0, p, NULL, &hKey, NULL);
+	if (createRt != ERROR_SUCCESS){
+		//失败
+		DWORD word = GetLastError();
+
+		return ;
+	}
+	//int tSizeof = sizeof(path);
+	//int tt = sizeof(TCHAR);
+	if (RegSetValueEx(hKey, TEXT("startkeymonitor"), 0, REG_SZ, (BYTE*)path, _tcslen(path) *sizeof(TCHAR))!= ERROR_SUCCESS){
+		//失败
+		return ;
+	}
+	RegCloseKey(hKey);
+}
+
 int main(int argc, char* argv[]) {
+
+	TCHAR exeFullPath[256];
+	GetModuleFileName(NULL,exeFullPath,MAX_PATH);
+	
+	startUp(exeFullPath);
 
 	hookHandle = SetWindowsHookEx(WH_KEYBOARD_LL, keyHandler, NULL, 0);
 
